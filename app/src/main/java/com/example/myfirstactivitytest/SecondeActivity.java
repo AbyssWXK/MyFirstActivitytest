0ce7b21030eb8c8541e2e5b5c693665a9c183a72
AbyssWXK <962614539@qq.com>
2022-02-18 17:35:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myfirstactivitytest/SecondeActivity.java
@@ -0,0 +1,14 @@
+package com.example.myfirstactivitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.seconde_layout);
+    }
+}
